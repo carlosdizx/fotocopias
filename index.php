831ad3801f995f30c0e1a667aa5f6ce87f7196ef
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-14 09:45:14 -0500

    Separada el codigo del index en los diferentes archivos, ademas, que los incluyo en el dicho index

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sistema de gestion de fotocopias - Universidad Mariana</title>
-</head>
-<body>
+<?php
+include("docs/header.php");
+include("docs/nav.php");
+?>
 
-</body>
-</html>
+<!-- Espacio de HTML -->
+
+<?php include("docs/footer.php"); ?>
