a2da5e312d79b9241d2e43f77698181da0a198e9
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-16 16:32:52 -0500

    agregado el campo de agregar programa academico

--- detalle_facultad.php
+++ detalle_facultad.php
@@ -13,37 +13,54 @@ if (isset($_GET['id'])) {
     <?php }
     else{ 
         ?>
-        <div class="container p-4">
         <h1>Programas academicos</h1>
+        <div class="container p-4">
+        <div class="row">
+            <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <form action="agregar_facultad.php" method="post" autocomplete="off">
+                        <div class="form-group">
+                            <input type="text" name="programa" placeholder="Ingrese el programa" required class="form-control" autofocus>
+                        </div>
+                        <br />
+                        <input class="btn btn-success" type="submit" value="Agregar" name="agregar">
+                    </form>
+                </div>
+            </div>
+            </div>  
 
-        <table class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>ID.</th>
-                        <th>Facultad</th>
-                        <th>Director</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    while($row=mysqli_fetch_array($consulta)){ ?>
-                    <tr>
-                        <td><?= $row['id'] ?></td>
-                        <td><?= $row['nombre'] ?></td>
-                        <td><?= $row['director'] ?></td>
-                        <td>
-                            <a href="" class="btn btn-warning">
-                                <span class="mdi mdi-pencil-outline">Editar</span>
-                            </a>
-                            <a href="" class="btn btn-danger">
-                                <span class="mdi mdi-trash-can-outline">Eliminar</span>
-                            </a>
-                        </td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-        </table>
+        <div class="col-md-8">
+            <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID.</th>
+                            <th>Facultad</th>
+                            <th>Director</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while($row=mysqli_fetch_array($consulta)){ ?>
+                        <tr>
+                            <td><?= $row['id'] ?></td>
+                            <td><?= $row['nombre'] ?></td>
+                            <td><?= $row['director'] ?></td>
+                            <td>
+                                <a href="" class="btn btn-warning">
+                                    <span class="mdi mdi-pencil-outline">Editar</span>
+                                </a>
+                                <a href="" class="btn btn-danger">
+                                    <span class="mdi mdi-trash-can-outline">Eliminar</span>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+            </table>
+        </div>
+        </div>
     </div>
      <?php } ?>
 <?php } ?>
