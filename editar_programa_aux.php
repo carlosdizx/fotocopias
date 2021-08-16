2bd170bedc3ac9ddc37c209b4af37491d275e77b
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-16 17:57:50 -0500

    full operaciones crud para programa

--- editar_programa_aux.php
+++ editar_programa_aux.php
@@ -1,1 +1,22 @@
 <?php
+include_once("docs/conexion.php");
+if(isset($_POST["actualizar"]) && isset($_GET['id']))
+{
+    $programa = $_POST['programa'];
+    $id = $_GET['id'];
+    $sql ="UPDATE programas SET nombre = '$programa' WHERE '$id' = id;";
+    $consulta = mysqli_query($conexion,$sql);
+    if (!$consulta){
+        die("La insercion no se pudo realizar ".$conexion->error);
+    }
+    else
+    {
+        $_SESSION['mensaje'] ="Programa academico actualizado correctamente";
+        $_SESSION['color'] ="success";
+        header("Location:/fotocopias/");
+    }
+}
+else
+{
+    echo "No llego";
+}
